29a389878146f01465556d1a21cb9755776a0680
FSFEASTER <171348471+FSFEASTER@users.noreply.github.com>
2026-01-20 17:54:41 +0100

    syntax error fix

--- popup.js
+++ popup.js
@@ -1,22 +1,16 @@
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  chrome.tabs.sendMessage(
-    tabs[0].id,
-    "GET_SELECTION",
-    (response) => {
-      const output = document.getElementById("output");
-      if (response) {
-        output.textContent = response;
-      } else {
-        output.textContent = "Kein Text markiert";
-      }
+  chrome.tabs.sendMessage(tabs[0].id, "GET_SELECTION", (response) => {
+    const output = document.getElementById("output");
+    if (response) {
+      output.textContent = response;
+    } else {
+      output.textContent = "Kein Text markiert";
     }
-  );
+  });
 });
-function fetchword(word){
+async function fetchword(word) {
   //wort fetchen
-  const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/${word}';
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/${word}";
   const response = await fetch(url);
   const data = await response.json();
-
-  })
 }
