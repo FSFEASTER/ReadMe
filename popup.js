bf2cbccdc5cc0b657946231f79e8603329bcc282
Prowl <171172724+Prowwll@users.noreply.github.com>
2026-01-22 10:08:44 +0100

    fetchword can now actually fetch the word and returns definition.

--- popup.js
+++ popup.js
@@ -1,16 +1,23 @@
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  chrome.tabs.sendMessage(tabs[0].id, "GET_SELECTION", (response) => {
+  chrome.tabs.sendMessage(tabs[0].id, "GET_SELECTION", async (response) => {
     const output = document.getElementById("output");
     if (response) {
-      output.textContent = response;
+      output.textContent = "loading definition..."
+      const definition = await fetchword(response);
+      output.textContent = definition
     } else {
       output.textContent = "Kein Text markiert";
     }
   });
 });
+
 async function fetchword(word) {
   //wort fetchen
-  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/${word}";
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   const response = await fetch(url);
-  const data = await response.json();
+  if(response.ok) {
+    const data = await response.json();
+    return data[0].meanings[0].definitions[0].definition;
+  }
+  
 }
