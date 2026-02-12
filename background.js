0f7d666aeb3a14c7f9c6994a72c3972f25d9a933
Prowl <171172724+Prowwll@users.noreply.github.com>
2026-02-12 01:17:58 +0100

    working on formatting api data

--- background.js
+++ background.js
@@ -1,14 +1,39 @@
 // Listens for script.js to send a word to look up
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "fetchDefinition") {
-    // Calls the API to get the desired data
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${request.word}`)
-      // HTTP response gets turned into JS object
-      .then((response) => response.json())
-      // Sends the dictionary result back / catches errors and sends an error message
-      .then((data) => sendResponse({ data }))
-      .catch((error) => sendResponse({ error: error.message }));
+    
+    (async () => {
+      try {
+        // Calls the API to get the desired data
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${request.word}`);
+
+        // HTTP response gets turned into JS object
+        const data = await response.json();
+
+        const definition1 = [];
+        const definition2 = [];
+        const synonyms = [];
+        const antonyms = [];
+        const entry = data[0];
+        const meanings = entry.meanings || [];
+
+        for (const meaning of meanings) {
+          for (const def of meaning.definitions || []) {
+            
+          }
+        }
+
+
+        // Sends the dictionary result back
+        sendResponse({data});
+        // catch error and send error message
+      }  catch (error) {
+        sendResponse({error: error.message});
+      }
+    })();
+
   }
+  
   return true; // Keeps the port open
 });
 
