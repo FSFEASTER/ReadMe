e889be2ffc7e3935ce540753d6160aa143fc9268
Prowl <171172724+Prowwll@users.noreply.github.com>
2026-03-05 11:45:47 +0100

    response array now consists of definition 1 and definition 2

--- background.js
+++ background.js
@@ -30,13 +30,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         if (meanings[1]){
           secondMeaning = meanings[1];
         }
+
         else if (data[1]?.meanings?.[0]){
-          secondMeaning = data[1].meanings
+          secondMeaning = data[1].meanings[0];
+        }
+
+        if(secondMeaning && secondMeaning.definitions){
+          const defs = secondMeaning.defintions;
+          definition2.push(
+            defs[0]?.definition ?? null,
+            defs[0]?.example ?? null,
+            defs[1]?.definition ?? null,
+            defs[1]?.example ?? null
+          );
+        }
+        else {
+          definition2.push(null, null, null, null)
         }
 
 
         // Sends the dictionary result back
-        sendResponse({data});
+        sendResponse({
+          definition1,
+          definition2
+        });
         // catch error and send error message
       }  catch (error) {
         sendResponse({error: error.message});
